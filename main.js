89de6b156b24a0b0893492b36c86c99e9db6f53e
Philip Spelman <philip.spelman@vaporstream.com>
2020-11-25 15:33:48 -0500

    - first changes, working through course

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 // Modules
 const {app, BrowserWindow} = require('electron')
 
+// Add the colors module
+const colors = require('colors');
+console.log(`colors: `, colors.rainbow("THESE ARE COLORS!"))
+
+// note: you can use native node modules in the npm process
+const bcrypt = require('bcrypt');
+bcrypt.hash('myPlaintextPassword', 10, function (err, hash) {
+    console.log(colors.rainbow(hash));
+});
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 // Create a new BrowserWindow when `app` is ready
-function createWindow () {
+function createWindow() {
 
-  mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
-    webPreferences: { nodeIntegration: true }
-  })
+    mainWindow = new BrowserWindow({
+        width: 1000, height: 800,
+        webPreferences: {nodeIntegration: true}
+    })
 
-  // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
+    // Load index.html into the new BrowserWindow
+    mainWindow.loadFile('index.html')
 
-  // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+    // Open DevTools - Remove for PRODUCTION!
+    mainWindow.webContents.openDevTools();
 
-  // Listen for window being closed
-  mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
+    // Listen for window being closed
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
 }
 
 // Electron `app` is ready
@@ -30,10 +40,10 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
 app.on('activate', () => {
-  if (mainWindow === null) createWindow()
+    if (mainWindow === null) createWindow()
 })
