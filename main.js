22f1c2ce84328398b5ccf00efd651f27f64abd90
Philip J Spelman <phil_spelman@hotmail.com>
2020-11-29 17:02:50 -0500

    - fixed indentation rule from 4 spaces to 2 spaces

--- main.js
+++ main.js
@@ -1,56 +1,63 @@
 // Modules
-const {app, BrowserWindow, Tray} = require('electron')
+const {app, BrowserWindow, Tray, Menu} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow, tray
 
+let trayMenu = Menu.buildFromTemplate([
+  {label: 'Item 1'},
+  {role: 'quit'},
+])
+
 function createTray() {
-    tray = new Tray('trayTemplate@2x.png')
-    tray.setToolTip('Tray details')
-    tray.on('click', e => {
-        // check for modifier key - if shift key is held down during the click, quit
-        if (e.shiftKey) {
-            app.quit();
-        } else {
-            mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();  // Note: this will toggle the visibility of the main window from tray icon click
-        }
-    })
+  tray = new Tray('trayTemplate@2x.png')
+  tray.setToolTip('Tray details')
+  tray.on('click', e => {
+    if (e.shiftKey) {  // check for modifier key - if shift key is held down during the click, quit
+      app.quit();
+    } else {
+      mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();  // Note: this will toggle the visibility of the main window from tray icon click
+    }
+  })
+  tray.setContextMenu(trayMenu)
 }
+
+
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
-    // create the tray icon
-    createTray()
+  // create the tray icon
+  createTray()
 
-    mainWindow = new BrowserWindow({
-        width: 900, height: 800,
-        x: 0, y: 0,
-        webPreferences: {nodeIntegration: true}
-    })
+  mainWindow = new BrowserWindow({
+    width: 900, height: 800,
+    x: 0, y: 0,
+    webPreferences: {nodeIntegration: true}
+  })
 
-    // Load index.html into the new BrowserWindow
-    mainWindow.loadFile('index.html')
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('index.html')
 
-    // Open DevTools - Remove for PRODUCTION!
-    // mainWindow.webContents.openDevTools();
+  // Open DevTools - Remove for PRODUCTION!
+  // mainWindow.webContents.openDevTools();
 
-    // Listen for window being closed
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    })
+  // Listen for window being closed
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 }
 
 // Electron `app` is ready
 app.on('ready', e => {
-    createWindow();
+  createWindow();
 })
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit()
 })
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
 app.on('activate', () => {
-    if (mainWindow === null) createWindow()
+  if (mainWindow === null) createWindow()
 })
