8344ea58e86ca149ec804176cd7b568367d1535b
Philip Spelman <philip.spelman@vaporstream.com>
2020-11-30 17:42:01 -0500

    - added examples of using the invoke and handle methods for IPC

--- main.js
+++ main.js
@@ -1,42 +1,70 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, dialog, ipcMain} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+async function askFruit() {
+  let fruits = ['apple', 'orange', 'grape']
+  let choice = await dialog.showMessageBox({  // this is initially a promise, but will fix in a moment
+    message: 'Pick a fruit',
+    buttons: fruits,
+  })
+  return fruits[choice.response]
+}
+
+// ipcMain.on('ask-fruit', e => {
+//     askFruit().then(answer => {
+//       // can finally send it back to the renderer using IPC
+//       console.log(`user picked: ${answer} | sending the selection to the renderer`)
+//       e.reply('answer-fruit', answer)
+//     })
+// })
+
+ipcMain.handle('ask-fruit', e=> {
+  return askFruit()
+})
+
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
 
-    mainWindow = new BrowserWindow({
-        width: 900, height: 800,
-        x: 0, y: 0,
-        webPreferences: {nodeIntegration: true}
-    })
+  // setTimeout(() => {
+  //     askFruit().then( answer => {
+  //         console.log(`answer: `, answer)
+  //
+  //     })
+  // }, 3000)
 
-    // Load index.html into the new BrowserWindow
-    mainWindow.loadFile('index.html')
+  mainWindow = new BrowserWindow({
+    width: 900, height: 800,
+    x: 0, y: 0,
+    webPreferences: {nodeIntegration: true}
+  })
 
-    // Open DevTools - Remove for PRODUCTION!
-    // mainWindow.webContents.openDevTools();
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('index.html')
 
-    // Listen for window being closed
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    })
+  // Open DevTools - Remove for PRODUCTION!
+  mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 }
 
 // Electron `app` is ready
 app.on('ready', e => {
-    createWindow();
+  createWindow();
 })
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit()
 })
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
 app.on('activate', () => {
-    if (mainWindow === null) createWindow()
+  if (mainWindow === null) createWindow()
 })
--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {ipcRenderer} = require('electron')
+
+document.getElementById('ask').addEventListener('click', e => {
+  // ipcRenderer.send('ask-fruit')
+  ipcRenderer.invoke('ask-fruit').then(answer => {
+    console.log(`answer: `, answer)
+  })
+})
+
+ipcRenderer.on('answer-fruit', (event, answer) => {
+  console.log(`answer: `, answer)
+})
