f9bf053e02faae681e23b25b4e3ff560899aa03e
Philip Spelman <philip.spelman@vaporstream.com>
2020-12-02 11:42:47 -0500

    - added placeholder for a read-item - added styling for the read-item class

--- renderer/app.js
+++ renderer/app.js
@@ -1,13 +1,44 @@
 // DOM nodes
 let showModal = document.getElementById('show-modal'),
   closeModal = document.getElementById('close-modal'),
-  modal = document.getElementById('modal')
+  modal = document.getElementById('modal'),
+  addItem = document.getElementById('add-item'),
+  itemUrl = document.getElementById('url'),
+  items = new Set()
+
+
+
+function clearUrlEntry() {
+  itemUrl.value = ""
+}
+
+// Modal interaction
+itemUrl.addEventListener('keyup', e => {  // allow enter key to trigger add
+  if(e.key === 'Enter') addItem.click()
+})
 
 showModal.addEventListener('click', e => {
-  console.log(`showing modal`, )
   modal.style.display = 'flex'
+  itemUrl.focus()  // note: focus on the input
 })
+
 closeModal.addEventListener('click', e => {
-  console.log(`hiding modal`, )
+  console.log(`hiding modal`,)
+  clearUrlEntry();
   modal.style.display = 'none'
+})
+
+
+// Handle new item
+addItem.addEventListener('click', e => {
+  if (itemUrl.value) {
+    // add the URL to the list of URLs
+    let newItem = itemUrl.value
+    console.log(`adding: `, newItem)
+    // clear the field
+    clearUrlEntry();
+    setTimeout(()=>{
+      alert(`${newItem} added to list`)
+    }, 1)
+  }
 })
