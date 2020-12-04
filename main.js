277122a5e5a4958066050457c11f3d6ce12bb750
Philip Spelman <philip.spelman@vaporstream.com>
2020-12-04 12:54:25 -0500

    - added basic touchbar functionality - implemented an addItem button - implemented toggle DevTools button

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules
-const {app, BrowserWindow, ipcMain} = require('electron')
+const {app, BrowserWindow, ipcMain, TouchBar} = require('electron')
 const windowStateKeeper = require('electron-window-state')
 const readItem = require('./readItem.js')
 const appMenu = require('./menu.js')
@@ -9,6 +9,50 @@ const updater = require('./updater.js')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+// Touchbar Label
+const tbLabel = new TouchBar.TouchBarLabel({
+  label: 'ReadIts',
+})
+
+// Touchbar button
+const tbAddNew = new TouchBar.TouchBarButton({
+  label: 'new readIt',
+  backgroundColor: '#0595DE',
+  click: () => {
+    mainWindow.send('menu-show-modal')
+  }
+})
+
+// DevTools button
+const tbDevTools = new TouchBar.TouchBarButton({
+  label: 'DevTools',
+  icon: `${__dirname}/file-code-solid@2x.png`,
+  iconPosition: 'left',
+  click: () => {
+    mainWindow.webContents.toggleDevTools()
+  }
+})
+
+// create a touchbar spacer
+// const tbSpacerSM = new TouchBar.TouchBarSpacer({size: 'small'})
+const tbSpacerSM = () => new TouchBar.TouchBarSpacer({size: 'small'})
+const tbSpacerLG = () => new TouchBar.TouchBarSpacer({size: 'large'})
+const tbSpacerFLX = () => new TouchBar.TouchBarSpacer({size: 'flexible'})
+// const tbSpacerLG = new TouchBar.TouchBarSpacer({size: 'large'})
+// const tbSpacerFLX = new TouchBar.TouchBarSpacer({size: 'flexible'})
+
+// Create the new touchbar
+const touchbar = new TouchBar({
+  items: [
+    tbLabel,
+    tbSpacerFLX(),
+    tbAddNew,
+    tbSpacerSM(),
+    tbDevTools,
+  ],  // touchbar components created from touchbar submodules
+  // escapeItem: null  // Note: this is for the case where the Escape key on the touchbar is to be replaced
+})
 
 // listen for new item request
 ipcMain.handle('new-item', (e, itemUrl) => {
@@ -43,6 +87,9 @@ function createWindow() {
 
   // Load main.html into the new BrowserWindow
   mainWindow.loadFile('renderer/main.html')
+
+  // Set touchbar on Mac
+  if (process.platform === 'darwin') mainWindow.setTouchBar(touchbar)
 
   // Open DevTools - Remove for PRODUCTION!
   // mainWindow.webContents.openDevTools();
